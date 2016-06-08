740f53f5d950050e81e7ab39a1d37018940f15a8
Udit Mittal <udit7590@yahoo.com>
2016-06-08 19:18:23 +0530

    Creates functions for finding matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
+## Takes a matrix a s parameter and returns a list of methods
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setInverseMatrix <- function(inverse_matrix) im <<- inverse_matrix
+        getInverseMatrix <- function() im
+        list(set = set, get = get,
+             setInverseMatrix = setInverseMatrix,
+             getInverseMatrix = getInverseMatrix)
 }
 
 
 ## Write a short comment describing this function
-
+# Takes special matrix created using `makeCacheMatrix` and finds inverse of the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cachedMatrix <- x$getInverseMatrix()
+        if(!is.null(cachedMatrix)) {
+                return(cachedMatrix)
+        } else {
+                x$setInverseMatrix(solve(x$get(), ...))
+                x$getInverseMatrix()
+        }
 }
